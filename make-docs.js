8e69f7d798bb5574a50ab5bff829d392fdcac889
Kristjan Broder Lund <kristjanlu@novomaticls.com>
2017-10-18 01:20:02 +0000

    Add popularity bar next to endpoints on docs page

--- make-docs.js
+++ make-docs.js
@@ -7,7 +7,10 @@ globby(['./docs/*.md', './endpoints/**/*.md', '!node_modules/**']).then(paths =>
   paths.forEach(path => {
     content += fs.readFileSync(path, 'utf8')
   })
-  const html = marked(content, { escapeMarkdown: false })
+  const body = marked(content, { escapeMarkdown: false })
+
+  const html = fs.readFileSync('./docs/index.html', 'utf8').replace('<Content />', body)
+
   fs.writeFileSync('./docs/dist/index.html', html, 'utf8')
 }).catch(error => {
   console.error(error)
